3a6b53cbb8d36952144df14c1dea3328d4d8b00e
akrajaei <52745077+akrajaei@users.noreply.github.com>
2022-02-28 12:05:03 +0100

    Add files via upload

---
+++ Ramsheh.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main () {
+
+	char ch;
+	float vfloat[];
+
+
+  std::ifstream myfile;
+  myfile.open ("myfile.txt", ios::in);
+
+  if (!myfile)
+	  exit(0);
+  
+  myfile.get(vfloat);
+  while(!myfile.oef())
+  {
+	  float calculate_median;
+	  float median_value;
+	  calculate_median = median_calculator(vfloat, median_value);
+  }
+
+
+  myfile.close();
+
+  return 0;
+}
+
+
+
+
+void sort(float f[], float len)
+{
+	float hold;
+
+	  for (float pass = 1; pas < len; pass++)
+		  for (float j = 0; j < len - a; j++)
+			  if (a [j] > a [j + 1] ){
+				  hold = a [j];
+	  	  	  	  a[j] = a [j +1];
+	  	  	  	  a[j + 1] = hold;
+				}
+}
+
+
+float median_calculator(float value_fromfile[], float len_from_sort)
+{
+	float temp_median;
+	temp_median = calculated_value = value_fromfile[len_from_sort/2];
+	return temp_median;
+}
+
